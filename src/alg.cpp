1a2b1a8840e024c00280cd03dece02f426fc6f66
mlukashov <125198939+mlukashov@users.noreply.github.com>
2023-03-25 23:28:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,59 +1,59 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int* arr, int len, int value)
 {
-	int kol = 0;
-	for (int i = 0; i < len; i++) {
-		for (int j = i+1; j < len; j++) {
-			if (arr[i] + arr[j] == value)
-				kol++;
-		}
-	}
-	return kol;
+    int kol = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i+1; j < len; j++) {
+            if (arr[i] + arr[j] == value)
+                kol++;
+            }
+        }
+    return kol;
 }
 int countPairs2(int* arr, int len, int value)
 {
-	int kol = 0;
-	len--;
-	while (arr[len] > value) {
-		len--;
-	}
-	for (int i = 0; i < len; i++) {
-		for (int j = len; j > i; j--) {
-			if (arr[i] + arr[j] == value) {
-				kol++;
-			}
-		}
-	}
-	return kol;
+    int kol = 0;
+    len--;
+    while (arr[len] > value) {
+        len--;
+    }
+    for (int i = 0; i < len; i++) {
+        for (int j = len; j > i; j--) {
+            if (arr[i] + arr[j] == value) {
+                kol++;
+            }
+        }
+    }
+    return kol;
 }
 int countPairs3(int* arr, int len, int value)
 {
-	int kol = 0;
-	for (int i = 0; i < len - 1; i++) {
-		int lev = i, prav = len;
-		while (lev + 1 < prav) {
-			int mid = (lev + prav) / 2;
-			if (arr[i] + arr[mid] > value) {
-				prav = mid;
-			}
-			else if (arr[i] + arr[mid] < value) {
-				lev = mid;
-			}
-			else {
-				kol++;
-				int a = mid + 1;
-				while (arr[i] + arr[a] == value && a < prav) {
-					kol++;
-					a++;
-				}
-				a = mid - 1;
-				while (arr[i] + arr[a] == value && a > lev) {
-					kol++;
-					a--;
-				}
-				break;
-			}
-		}
-	}
-	return kol;
+    int kol = 0;
+    for (int i = 0; i < len - 1; i++) {
+        int lev = i, prav = len;
+        while (lev + 1 < prav) {
+            int mid = (lev + prav) / 2;
+            if (arr[i] + arr[mid] > value) {
+                prav = mid;
+            }
+            else if (arr[i] + arr[mid] < value) {
+                lev = mid;
+            }
+            else {
+                kol++;
+                int a = mid + 1;
+                while (arr[i] + arr[a] == value && a < prav) {
+                    kol++;
+                    a++;
+                }
+                a = mid - 1;
+                while (arr[i] + arr[a] == value && a > lev) {
+                    kol++;
+                    a--;
+                }
+                break;
+            }
+        }
+    }
+    return kol;
 }
